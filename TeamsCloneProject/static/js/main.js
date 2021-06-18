09b6445372a64356eb6f140007454e2859db0422
satvikakoti <55699106+satvikakoti@users.noreply.github.com>
2021-06-18 22:57:07 +0530

    Added new

--- TeamsCloneProject/static/js/main.js
+++ TeamsCloneProject/static/js/main.js
@@ -327,3 +327,14 @@ function getDataChannels(){
 
     return dataChannels;
 }
+
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+}
