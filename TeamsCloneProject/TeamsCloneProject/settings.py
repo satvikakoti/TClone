128bbadbe540d5138e6b8d38cea96e9a1a8bb1ab
satvikakoti <55699106+satvikakoti@users.noreply.github.com>
2021-06-25 16:49:42 +0530

    Update settings.py

--- TeamsCloneProject/TeamsCloneProject/settings.py
+++ TeamsCloneProject/TeamsCloneProject/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = 'C:/Python/ProgramsPython/HelloWorld/venv/TeamsCloneProject/static'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
