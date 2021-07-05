de9d45aafe704f4882f0f257225948c5bb717aea
satvikakoti <55699106+satvikakoti@users.noreply.github.com>
2021-07-05 18:36:51 +0530

    Update settings.py

--- TeamsCloneProject/TeamsCloneProject/settings.py
+++ TeamsCloneProject/TeamsCloneProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['https://msteamclone.herokuapp.com/']
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chat',
-    'channels',
+
 ]
 
 MIDDLEWARE = [
