7c11a45d4546219709b2bd486e8e41d593c3e8f5
satvikakoti <55699106+satvikakoti@users.noreply.github.com>
2021-07-07 11:40:43 +0530

    Update asgi.py

--- TeamsCloneProject/TeamsCloneProject/asgi.py
+++ TeamsCloneProject/TeamsCloneProject/asgi.py
@@ -13,7 +13,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TeamsCloneProject.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
