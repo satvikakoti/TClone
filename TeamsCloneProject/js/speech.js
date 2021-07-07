4b22b463c00e9465d570f848096c51a326157e26
satvikakoti <55699106+satvikakoti@users.noreply.github.com>
2021-07-07 18:48:51 +0530

    Update speech.js

--- TeamsCloneProject/js/speech.js
+++ TeamsCloneProject/js/speech.js
@@ -1,12 +1,24 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
 
 var recognition = new SpeechRecognition();
-
+var reply;
 var Textbox;
-//var Textbox = document.getElementById('textbox');
-//var instructions = document.getElementById('instructions');
-
+var msg;
+var voices;
 var Content = '';
+
+if ('speechSynthesis' in window)
+{
+       msg = new SpeechSynthesisUtterance();
+       voices = window.speechSynthesis.getVoices();
+       msg.voice = voices[1];
+       msg.rate = 0.8;
+       msg.pitch = 1;
+}
+else
+{
+        alert('Speech synthesis isn\'t supported.');
+}
 
 recognition.continuous = true;
 
@@ -15,66 +27,64 @@ recognition.onresult = function(event) {
 var current = event.resultIndex;
 
 var transcript = event.results[current][0].transcript;
-
-    Content += transcript;
+//    console.log(transcript);
+    var n = transcript.split(" ");
+    Content = n[n.length - 1];
     Textbox = Content;
-//    Textbox.innerHTML=Content;
-          if ('speechSynthesis' in window) {
-            var msg = new SpeechSynthesisUtterance();
-            var voices = window.speechSynthesis.getVoices();
-            msg.voice = voices[1];
-            msg.rate = 0.8;
-            msg.pitch = 1;
-            msg.text = Content;
-            speechSynthesis.speak(msg);
-          }
-          else{
-               alert('Speech synthesis isn\'t supported.');
-          }
+    if(Content=="yes")
+    {
+        var newContent = askJoinRoom();
+//        if(newContent=="yes")
+            location.href = 'video/';
+//        else{
+//            msg.text = 'Bye! See you!';
+//            speechSynthesis.speak(msg);
+//        }
 
+    }
+    else if(Content=="no")
+    {
+            msg.text = 'Okay! Have a great day!';
+            speechSynthesis.speak(msg);
+    }
+    document.getElementById('start-btn').disabled =false;
+    recognition.stop();
 };
 
-recognition.onstart = function() {
-  document.getElementById('start-btn').disabled =false;
-//  instructions.innerHTML='Voice recognition is ON.';
-  document.getElementById('start-btn').innerHTML = 'Stop';
-  document.getElementById('start-btn').addEventListener('click',()=>{
-
-        document.getElementById('start-btn').innerHTML = 'Start';
-//        instructions.innerHTML='Speech recognition ended';
-        recognition.stop();
-  });
+recognition.onstart = function(event)
+{
+  recognition.stop();
 }
 
 recognition.onspeechend = function (){
-//  instructions.innerHTML='No activity.';
   document.getElementById('start-btn').innerHTML = 'Start over again';
   document.getElementById('start-btn').disabled =false;
-//  recognition.stop();
+  recognition.stop();
+  recognition.start();
 }
 
 recognition.onerror = function(event) {
   if(event.error == 'no-speech') {
-//    instructions.innerHTML='Try again.';
+    console.log('Error!');
+    recognition.stop();
   }
 }
 
-document.getElementById('start-btn').addEventListener ('click', () => {
+document.getElementById('start-btn').addEventListener ('click', () =>
+{
+            msg.text = 'Hello! Welcome to T Clone! Would you like to try out voice assistant?';
+            Content=msg;
+            speechSynthesis.speak(msg);
+
   if (Content.length) {
     Content = '';
   }
-   if ('speechSynthesis' in window) {
-        var msg = new SpeechSynthesisUtterance();
-        var voices = window.speechSynthesis.getVoices();
-            msg.voice = voices[1];
-            msg.rate = 0.8;
-            msg.pitch = 1;
-            msg.text = 'Hello! Welcome to T Clone! Would you like to try out voice assistant?';
-            speechSynthesis.speak(msg);
-       }
   recognition.start();
 });
 
-//Textbox.addEventListener('input', function() {
-//  Content = this.val();
-//})
+
+function askJoinRoom()
+{
+            msg.text = 'Alright! Let me describe about T Clone. TClone is a simple video calling application with various features that you might like. Let me join you into a room right away!';
+            speechSynthesis.speak(msg);
+}
