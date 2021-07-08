bfa66b65dba8f6ad99cd7d47122d5eb09d4df745
Ayush Gupta <ayushgupta0010@gmail.com>
2021-07-08 17:27:40 +0530

    removed django_heroku

--- core/settings.py
+++ core/settings.py
@@ -1,8 +1,6 @@
 import os
 from pathlib import Path
 
-import django_heroku
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,5 +130,3 @@ CHANNEL_LAYERS = {
         },
     },
 }
-
-django_heroku.settings(locals())
