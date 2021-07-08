e9cf3c0ca137cd720f04ef482ccf0e91775c1726
satvikakoti <55699106+satvikakoti@users.noreply.github.com>
2021-07-08 11:43:22 +0530

    Delete gunicorn.conf.py

--- gunicorn.conf.py
+++
@@ -1,3 +0,0 @@
-# <module-path> is the relative path to the folder that contains the module
-# that contains wsgi.py; <module> is the name of the folder containing wsgi.py.
-gunicorn --bind=0.0.0.0 --timeout 1800 --workers=1 --chdir TeamsCloneProject/ TeamsCloneProject.wsgi
